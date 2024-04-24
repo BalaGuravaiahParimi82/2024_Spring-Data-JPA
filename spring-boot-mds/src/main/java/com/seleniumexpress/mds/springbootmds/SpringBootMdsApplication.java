7ab8ae245b53a843fc764c9e94b1e0939f2e1e06
Bala Guravaiah <balaguravaiahparimi3@gmail.com>
2024-04-24 20:44:37 +0530

    sample app

---
+++ spring-boot-mds/src/main/java/com/seleniumexpress/mds/springbootmds/SpringBootMdsApplication.java
@@ -0,0 +1,13 @@
+package com.seleniumexpress.mds.springbootmds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMdsApplication.class, args);
+	}
+
+}
---
+++ spring-boot-mds/src/test/java/com/seleniumexpress/mds/springbootmds/SpringBootMdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seleniumexpress.mds.springbootmds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
